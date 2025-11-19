05e4ce7bef5e55922b04c25aa8239618c9ecb2ec
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-11-19 22:06:34 +0700

    feat: submit Lab4

---
+++ Lab4/src/components/Bai3.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- Hiển thị lời chào -->
+     <h1>{{ greeting }}</h1>
+
+     <!-- Tạo nút thay đổi nội dung -->
+      <button v-bind:title="buttonTitle" @click="changeGreeting">Click để thay đổi</button>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                greeting: 'Xin chào bạn!',
+                buttonTitle: 'Nhấn vào đây để thay đổi lời chào'
+            };
+        },
+        methods: {
+            changeGreeting() {
+                this.greeting = 'Hello world!';
+                this.buttonTitle = 'Lời chào mới đã được cập nhập';
+            }
+        }
+    }
+</script>
