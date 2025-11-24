fa4de2eeaf3e91f3644e7c9476ba893df49349c2
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-11-24 20:50:55 +0700

    Feat: Submit Lab5

---
+++ Lab5/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+import Bai1 from './components/Bai1.vue'; 
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
+import CreatePost from './components/CreatePost.vue';
+import PostList from './components/PostList.vue';
+
+const posts = ref([]);
+
+function addPost(post) {
+  posts.value.push(post);
+}
+</script>
+<template>
+  <div class="container">
+     <Bai1/>
+      <Bai2/>
+      <Bai3/>
+
+      <!-- Bài 4 -->
+      <div id="app" class="container">
+        <h1>Ứng dụng Blog nhỏ với Vue.js</h1>
+        <CreatePost @add-post="addPost" />
+        <PostList :posts="posts" />
+      </div>
+  </div>
+
+ 
+</template>
+
+<style scoped>
+#app { max-width: 800px; margin: 0 auto; padding: 20px; }
+</style>
---
+++ Lab5/src/components/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Danh sách bài viết</h2>
+    <div 
+      v-for="(post, index) in posts" 
+      :key="index" 
+      :class="{ 'highlighted': post.title.length > 20 }" 
+      class="post"
+    >
+      <h3 class="text-success">{{ post.title }}</h3>
+      <h4 class="text-danger">{{ post.author }}</h4>
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  posts: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+.post { padding: 20px; margin: 10px 0; border: 1px solid #ddd; border-radius: 10px; }
+.highlighted { border: 2px solid lightgray; background-color: #f9f9f9; }
+</style>
