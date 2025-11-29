477f6695ac555bb917bbad7cdeb2da6620d75951
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-11-29 21:31:43 +0700

    Update Lab1: Fix bug Exception Hanlding

--- Lab6/src/components/Bai1.vue
+++ Lab6/src/components/Bai1.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="container mt-4">
         <h3>Nhập điểm trung bình của bạn:</h3>
-        <input type="number" v-model.number="score" min="0" max="10" class="form-control w-25 mb-3">
+        <input type="number" v-model.number="score" class="form-control w-25 mb-3">
 
-        <p class="fs-4 fw-bold text-success" v-if="score >= 9.0">Xuất sắc</p>
+        <p class="fs-4 fw-bold text-dark" v-if="score > 10 || score < 0">
+            Vui lòng nhập điểm hợp lệ (0 - 10)!
+        </p>
+
+        <p class="fs-4 fw-bold text-success" v-else-if="score >= 9.0">Xuất sắc</p>
         <p class="fs-4 fw-bold text-primary" v-else-if="score >= 8.0">Giỏi</p>
         <p class="fs-4 fw-bold text-info" v-else-if="score >= 6.5">Khá</p>
         <p class="fs-4 fw-bold text-warning" v-else-if="score >= 5.0">Trung bình</p>
-        <p class="fs-4 fw-bold text-danger" v-else-if="score >= 0">Yếu</p>
-        <p class="fs-4 fw-bold text-dark" v-else>Vui lòng nhập điểm hợp lệ!</p>
+        
+        <p class="fs-4 fw-bold text-danger" v-else-if="score !== null">Yếu</p>
     </div>
 </template>
 
