e57ffbef1650e37eb5c8a5caf6b6a2290a68801b
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-12-05 12:28:36 +0700

    add Slide7

---
+++ Slide7/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Slide7 from './components/Slide7.vue';
+import { ref } from 'vue';
+const numberMain = ref(null);
+const handleEvent = (number) => {
+  numberMain.value = number;
+};
+
+const showMessage = () => {
+  alert(messager);
+};
+</script>
+
+<template>
+  <Slide7 @myEvent="handleEvent" />
+  <h3>Giá trị gửi từ Component con: {{ numberMain }} * 2 = {{ numberMain * 2 }}</h3>
+</template>
+  
+<style scoped>
+
+</style>
---
+++ Slide7/src/components/Slide7.vue
@@ -0,0 +1,21 @@
+
+<!-- Nhập số trong component con
+ Gửi số ra App.vue: nhân đôi -> xuất ra màn hình -->
+<template>
+    <input type="number" v-model="myNumber">
+    <h4>{{ myNumber }}</h4>
+    <button @click="sendNumber">Gửi</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const myNumber = ref(0);
+const emit = defineEmits(['myEvent']);
+const sendNumber = () => {
+    emit('myEvent', myNumber.value);
+}
+const showMessage = () => {
+  alert(messager);
+};
+</script>
+
