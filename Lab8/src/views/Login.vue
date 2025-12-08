0448ed0e8a117f1d955205fb3bc63ed10190005e
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-12-08 20:02:59 +0700

    Add Login page

---
+++ Lab8/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Đăng nhập</h2>
+    <div>
+      Tên đăng nhập: <input type="text" /> Mật khẩu: <input type="text" />
+    </div>
+    <br />
+    <button @click="login">Đăng nhập (giả lập)</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function login() {
+  const success = true;
+  if (success) {
+    localStorage.setItem("isAuthenticated", "true");
+    router.push({ name: "Dashboard" });
+  }
+}
+</script>
