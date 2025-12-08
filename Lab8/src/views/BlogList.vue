6fd94e59ca16eeb32131a878d741693513215d79
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-12-08 20:01:56 +0700

    Add Blog page

---
+++ Lab8/src/views/BlogList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Danh sách bài viết Blog</h2>
+    <ul>
+      <li v-for="post in blogPosts" :key="post.id">
+        <router-link :to="{ name: 'BlogPost', params: { id: post.id } }">
+          {{ post.title }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const blogPosts = ref([
+  { id: "1", title: "Bài viết số 1" },
+  { id: "2", title: "Bài viết số 2" },
+  { id: "3", title: "Bài viết số 3" },
+]);
+</script>
---
+++ Lab8/src/views/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Chi tiết bài viết Blog</h2>
+    <p>ID bài viết: {{ postId }}</p>
+    <p>Nội dung của bài viết với ID {{ postId }} sẽ được hiển thị ở đây.</p>
+    <router-link :to="{ name: 'BlogList' }"
+      >Quay lại danh sách Blog</router-link
+    >
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import { ref, watch } from "vue";
+
+const route = useRoute();
+const postId = ref(route.params.id);
+
+watch(
+  () => route.params.id,
+  (newId) => {
+    postId.value = newId;
+  }
+);
+</script>
