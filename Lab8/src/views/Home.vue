f7276c908a86513fe4a9f92cb02e7496647cb1bd
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-12-08 20:00:40 +0700

    Add Home page

---
+++ Lab8/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Chào mừng đến với MyBlog!</h2>
+    <p>Đây là trang chủ của bạn.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
