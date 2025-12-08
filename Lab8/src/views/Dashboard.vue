fd27c04bcc259af3948e5626c010769d0814996e
Le Quang Huy <quanghuy.le.dev@gmail.com>
2025-12-08 20:02:28 +0700

    Add Dashboard

---
+++ Lab8/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Bảng điều khiển</h2>
+    <p>Chào mừng đến với trang chỉ dành cho người dùng đã đăng nhập!</p>
+    <p>Bạn đã đăng nhập thành công.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
